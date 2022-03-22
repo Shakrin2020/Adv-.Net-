85232e50e216114ca1503fac87678dfd9365c88c
Shakrin2020 <shakirinjahan@gmail.com>
2022-03-23 01:15:25 +0600

    final assignment

---
+++ APIassignment/APIassignment/Controllers/DepartmentController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using APIassignment.Models.Database;
+
+namespace APIassignment.Controllers
+{
+    public class DepartmentController : ApiController
+    {
+        // GET api/<controller>
+        [Route("api/get/dept")]
+        [HttpGet]
+        public List<Department> Get()
+        {
+            apidbEntities db = new apidbEntities();
+
+            var depts = new List<Department>();
+
+            foreach (var d in db.Departments)
+            {
+                var dt = new Department()
+                {
+                    D_Id = d.D_Id,
+                    D_Name = d.D_Name,
+                   
+                };
+
+                depts.Add(dt);
+            }
+
+            return depts;
+        }
+        // POST api/<controller>
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ APIassignment/APIassignment/Controllers/StudentController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using APIassignment.Models.Database;
+
+namespace APIassignment.Controllers
+{
+    public class StudentController : ApiController
+    {
+        // GET api/<controller>
+
+        [Route("api/get/student")]
+        [HttpGet]
+        public List<Student> Get()
+        {
+            apidbEntities db = new apidbEntities();
+
+            var students = new List <Student>();
+
+            foreach (var s in db.Students)
+            {
+                var stu = new Student()
+                {
+                    S_Id = s.S_Id,
+                    S_Name = s.S_Name,
+                    S_Dob = s.S_Dob,
+                };
+
+                students.Add(stu);
+            }
+           
+            return students;
+        }
+       
+        // POST api/<controller>
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
